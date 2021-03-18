6068d4721abbb774b4483dbe5547ce230857fe33
Maximiliano Farias <maximilianofarias88@gmail.com>
2021-03-18 20:07:23 -0300

    barra navegadora y slide de fotos

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css" >
+    <script src="https://kit.fontawesome.com/db8a4d38a8.js" crossorigin="anonymous"></script>
+    <title>Tandil Turismo</title>    
+</head>
+<body>
+<main>  
+<header>
+  <nav class="contenedor_nav">
+    <nav class="contenedor_sup">
+      <div class="redes">
+        <div><a href="#">home</a></div>
+        <div><a href="#"><i class="fab fa-whatsapp"></i></a></div>
+        <div><a href="#"><i class="fab fa-instagram"></i></a></div>
+        <div><a href="#"><i class="fab fa-facebook-square"></i></a></div>
+        <div><a href="#"><i class="fab fa-twitter"></i></a></div>
+      </div> 
+      <div id="info">
+        <a href="#">informaciónImportante_1</a>
+        <a href="#">informaciónImportante_2</a>
+        <a href="#">informaciónImportante_3</a> </div>
+    </nav>
+    <nav class="banner">
+      <div><a href="#"><img src="img/ttandil_2.jpg"></a></div>
+      <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2> 
+      <nav class="barra">
+       <ul>
+           <li class="nivel_1"><a href="#">INFORMATE</a>
+               <ul>
+                   <a href="#"><li class="nivel_2">opcion_uno</li></a>
+                   <a href="#"><li class="nivel_2">opcion_dos</li></a>
+                   <a href="#"><li class="nivel_2">opcion_tres</li></a>
+               </ul></li>    
+           <li class="nivel_1"><a href="#">PAQUETES</a>
+               <ul>
+                   <a href="#"><li class="nivel_2">opcion_uno</li></a>
+                   <a href="#"><li class="nivel_2">opcion_dos</li></a>
+                   <a href="#"><li class="nivel_2">opcion_tres</li></a>
+               </ul></li>   
+           <li class="nivel_1"><a href="#">LO NUESTRO</a>
+               <ul>
+                   <a href="#"><li class="nivel_2">opcion_uno</li></a>
+                   <a href="#"><li class="nivel_2">opcion_dos</li></a>
+                   <a href="#"><li class="nivel_2">opcion_tres</li></a>
+               </ul></li>   
+           <li class="nivel_1"><a href="#">PROMOCIONES</a>
+               <ul>
+                   <a href="#"><li class="nivel_2">opcion_uno</li></a>
+                   <a href="#"><li class="nivel_2">opcion_dos</li></a>
+                   <a href="#"><li class="nivel_2">opcion_tres</li></a>
+               </ul></li>   
+           <li class="nivel_1"><a href="#">QUÉ VISITAR</a>
+               <ul>
+                   <a href="#"><li class="nivel_2">opcion_uno</li></a>
+                   <a href="#"><li class="nivel_2">opcion_dos</li></a>
+                   <a href="#"><li class="nivel_2">opcion_tres</li></a>
+               </ul></li>   
+        </ul>
+      </nav>              
+    </nav>     
+  </nav>
+ </header>  
+    <section class="contenedor_slide">
+      <img src="img/slide_0.jpg">
+       <ul>
+           <li id="select_uno"><a href="#uno"><p>#paisaje_uno</p></a>
+               <ul id="uno">
+                   <li id="uno"><img src="img/slide_1.jpg" id="uno"></li>
+               </ul></li>
+           <li id="select_dos"><a href="#dos"><p>#paisaje_dos</p></a>
+               <ul>
+                   <li id="dos"><img src="img/slide_2.jpg"></li>
+               </ul></li>
+           <li id="select_tres"><a href="#tres"><p>#paisaje_tres</p></a>
+               <ul id="tres">
+                   <li ><img src="img/slide_3.jpg"></li>
+               </ul></li>    
+       </ul>    
+    </section> 
+    <section class="lugares">
+       <div></div>
+       <div></div>
+       <div></div>
+    </section>   
+  </main>     
+</body>
+</html>
